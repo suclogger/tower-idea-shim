c744d8e2d462705c554660d5be27ebe96cc5e48c
pufang runqiang.crq <runqiang.crq@cainiao.com>
2019-01-10 19:50:05 +0800

    upgrade tower version

--- install.sh
+++ install.sh
@@ -1,3 +1,3 @@
-mkdir -p ~/Library/Application\ Support/com.fournova.Tower2/CompareTools
-cp CompareTools.plist idea-tower.sh ~/Library/Application\ Support/com.fournova.Tower2/CompareTools
-chmod +x ~/Library/Application\ Support/com.fournova.Tower2/CompareTools/idea-tower.sh
+mkdir -p ~/Library/Application\ Support/com.fournova.Tower3/CompareTools
+cp CompareTools.plist idea-tower.sh ~/Library/Application\ Support/com.fournova.Tower3/CompareTools
+chmod +x ~/Library/Application\ Support/com.fournova.Tower3/CompareTools/idea-tower.sh
